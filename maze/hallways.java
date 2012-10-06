2afef36e8ad5f3cb60268e2ed63a7cd668f2a571
Ninjapancakes87 <inhoppancakes@gmail.com>
2012-10-06 13:40:57 -0400

    Initial Upload
    in hallways.java, in the void start(), in my switch loop it gives me a
    "cannot make a static reference" error

---
+++ maze/hallways.java
@@ -0,0 +1,21 @@
+package maze;
+
+public class hallways extends FMaze{
+	public static void main(String[] args){FMaze Hallways = new FMaze();}
+	public void hallway1()
+	{
+		int test = 0;
+		System.out.println("So, you chose hallway one.  You have no idea what is ahead....");
+		this.guard();
+	}
+	public void hallway2()
+	{
+		System.out.println("Good choice.  You chose hallway two.  Ahead is clear skies and warm, sunny days.");
+		this.desert();
+	}
+	public void hallway3()
+	{
+		System.out.println("Funny.  You chose hallway three.");
+		this.snakes();
+	}
+}
